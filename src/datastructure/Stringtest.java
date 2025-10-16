025e7642aa6adeaf9f6f6cef97d110f3659d7152
Ruben <davidruizglobal30@gmail.com>
2025-10-16 10:24:07 +0200

    16/10/2025

--- src/datastructure/Stringtest.java
+++ src/datastructure/Stringtest.java
@@ -4,8 +4,11 @@ import java.util.Arrays;
 
 public class Stringtest {
 	public static void main(String[] args) {
+		String querysql = "selec * from user where username = 'Ruben' and email  = 'manolo@gmail.com'";
+		querysql = "select *from user where username = ? and email = ?";
 		String[] params = { "manolo", "dgmail.com" };
-		selectquery(params, "Ruben", "d");
+		sqlProcess(params, querysql);
+
 	}
 
 	/**
@@ -15,16 +18,15 @@ public class Stringtest {
 	 * @param email
 	 * @return String
 	 */
-	private static String selectquery(String[] parameters, String username, String email) {
-		String querysql = "selec * from user where username = 'Ruben' and email  = 'manolo@gmail.com'";
-		querysql = "select *from user where username = ? and email = ?";
+	private static String sqlProcess(String[] parameters, String querysql) {
+
 		char[] characters = querysql.toCharArray();
 		int i = 0;
 
 		String sumString = "";
 
 		for (char character : characters) {
-//			System.out.println("the current character is " + character);
+//			System.out.println("the current character is " + character);rtth
 //			relational operator of inequality is "!=" "no igual "
 			if (character != '?') {
 //				"+=" operator to connect 
@@ -38,13 +40,13 @@ public class Stringtest {
 		System.out.println("sub String" + querysql.subSequence(0, 10));
 
 		System.out.println(" replace all ? with the value passed thorught the username variable "
-				+ querysql.replace("?", username));
+				+ querysql.replace("?", "dsds"));
 
 		System.out.println(Arrays.toString(characters));
 		System.out.println(querysql.indexOf("?"));
 		System.out.println(querysql.charAt(35));
 		System.out.println(querysql.charAt(3));
-		return "";
+		return querysql;
 
 	}
 }
