f5ea1ca45ab6c0a1732fe72a2e081a8c76ab6585
Ruben <davidruizglobal30@gmail.com>
2025-11-18 13:04:47 +0100

    implementation of add an algorith in flexible array 18/11/2025

--- src/datastructure/FexibleArraytest.java
+++ src/datastructure/FexibleArraytest.java
@@ -2,9 +2,13 @@ package datastructure;
 
 public class FexibleArraytest {
     public static void main(String[] args) {
-        FlexibleArray<Object> flexileArray = new FlexibleArray<>();
+        FlexibleArray<Object> flexileArray = new FlexibleArray<>(1);
        //git  System.out.println("the current sie =" flexileArray.size());
-        flexileArray.add(3);
-        flexileArray.remove(4);
+      System.out.println("the current size = " + flexileArray.size());
+      for (int i = 0; i < 6; i++) {
+          flexileArray.add(3);
+
+	}
+      flexileArray.remove(4);
     }
 }
--- src/datastructure/FlexibleArray.java
+++ src/datastructure/FlexibleArray.java
@@ -1,49 +1,65 @@
 package datastructure;
+
 /*
 * This is a class that represent an extensible array
 * An aaray has a fixed capacity, when ranching the end
 * of the array, we have to expand it automaticaly*/
-public class FlexibleArray <T> {
-    private T[] elements;
-    private static final int initial_capacity = 5;
+public class FlexibleArray<T> {
+	private T[] elements;
+	private static final int initial_capacity = 5;
+	private int size = 0;
+	private int expansion_facor = 40;
+
 //the variable to sotore how many elemets we have added to the array
-    public FlexibleArray() {
-        elements = (T[]) new Object[initial_capacity];
-    }
+	public FlexibleArray() {
+		elements = (T[]) new Object[initial_capacity];
+	}
 
-    public FlexibleArray(int size) {
-        elements = (T[]) new Object[size];
-    }
-    /*
-     * add an element to the array at its last position
-     * @param e
-     * */
+	public int size() {
 
-    public void add(T i){
-       //paso 0: check if we have reached to the last position
+		return elements.length;
+	}
 
-        // if true, we have to expand the capacity of the array
+	public FlexibleArray(int size) {
+		if (size <= 0) {
 
-        // paso 1: add the element to the last free position of the array
-        for (int j = 0; j < elements.length; j++) {
-            if (elements[j] == null) {
-                elements[j] = i;
-                return;
-            }
-        }
+			size = initial_capacity;
+		}
+		elements = (T[]) new Object[size];
+	}
+	/*
+	 * add an element to the array at its last position
+	 * 
+	 * @param e
+	 */
 
-        }
-        /*
-         * remove an element to the array at its last position
-         * @param i
-         * */
-        public void remove ( int i){
+	public void add(T element) {
+		
+		// paso 0: check if we have reached to the last position
 
-        }
-        public int size(){
+		// if true, we have to expand the capacity of the array
+		if (size == elements.length) {
+			T[] newArray = (T[]) new Object[elements.length + expansion_facor];
+			// copy the elements that from the old array to the new one
+			for (int i = 0; i < newArray.length; i++) {
+				newArray[i] = elements[i];
+			}
+			
+			elements = newArray;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
+		}
 
+		// paso 1: add the element to the last free position of the array
+		elements[size] = element;
+		size++;
+	}
 
-            return elements.length;
-        }
-    }
+	/*
+	 * remove an element to the array at its last position
+	 * 
+	 * @param i
+	 */
+	public void remove(int i) {
 
+	}
+
+}
