3cc44589e091dbe7619ff2a05e1d08ae5f11b771
Ruben <davidruizglobal30@gmail.com>
2025-11-26 14:39:47 +0100

    eclipse actualizado dia 26/11/2025 implemetnacion contact

--- src/oopmodeling/addressbook/ContactApp.java
+++ src/oopmodeling/addressbook/ContactApp.java
@@ -1,11 +1,82 @@
 package oopmodeling.addressbook;
 
+
+import java.util.Scanner;
+
 public class ContactApp {
-public static void main(String[] args) {
-	AddressBook addressBook = new AddressBook();
-	addressBook.showContacts();
-	addressBook.removeContact(addressBook);
+	 final static AddressBook addressBook = new AddressBook();
+	 final static Scanner scanner = new Scanner(System.in);
+
 	
+public static void main(String[] args) {
+	byte option = 4;
+	
+	while(true) {
+		showMenu();
+		
+		option = scanner.nextByte();
+		scanner.nextLine(); 
+
+		System.out.println(option);
+		
+		
+		
+		
+		if (option == 4) {
+			System.out.println("Vas a salir del programa");
+			break;
+		}
+		else if (option == 1) {
+			
+			addContact();
+
+	}
+		
+	
+	}
+	
+	
+//	addressBook = new AddressBook();
+//	addressBook.showContacts();
+//	addressBook.removeContact(null);
+	
+	
+	
+	
+}
+
+
+private static void addContact() {
+	System.out.println("-----add contact to your address book------");
+//1 the user introduce the number, name and other information about the contact
+	System.out.print("Introduce the name: ");
+    String name = scanner.nextLine();
+
+    System.out.print("Introduce the phone number: ");
+    String phone = scanner.nextLine();
+
+    System.out.print("Introduce the email: ");
+    String email = scanner.nextLine();
+//	2 create an object of "contact" encapsulating all the data passed in the first step
+    
+
+ 
+//	3 Add the new object to the contact array
+   
+
+
+}
+
+
+
+
+
+private static void showMenu() {
+System.out.println("1. Añadir contacto");	
+System.out.println("2. Enseñar contacto");	
+System.out.println("3. Eliminar contacto");	
+System.out.println("4.Exit");	
+
 }
 	
 	
