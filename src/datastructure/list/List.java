3b4a5321855d4e5bbdbf534605729616268e90e2
Ruben <davidruizglobal30@gmail.com>
2026-02-13 11:38:18 +0100

    dia 13/02/2026 ejercicio nodos

--- src/datastructure/list/List.java
+++ src/datastructure/list/List.java
@@ -1,23 +1,54 @@
 package datastructure.list;
 
-public class List <E>{
-	
-	public class Mode  {
-//		The data stored in a mode of the list
-		private E e;
-///each node needsto point to the next node, we use this variable to do it
-		private Mode  next;
-		}
+public class List<E> {
 
 //	The refernces to the last element in the list
-	
-	private Mode last;
-//	 The refences o the first element of ther list
-	
-	private Mode first;
 
-	public void add(Mode node) {
-// 1	
-		last.next = node;
+	private Node<E> last;
+//	 The refences o the first element of ther list
+
+	private Node<E> first;
+
+	public void add(Node<E> node) throws Exception {
+// 1	check the parameter if it valid
+		if (node == null) {
+			throw new Exception("you cannot pass anull node to the list");
+		}
+//	2	 add the node to the tail of the list	
+//	2.1	Check first if the head element is null
+//		is the case of null, it means the list is empty
+		if (first == null) {
+//			The list is empty
+			last = node;
+			first = node;
+			first.next = node;
+		}
+		else {
+			last.next = node;
+			last = node;
+
+		}
+		
 	}
+
+	public Node<E> getLast() {
+		return last;
+	}
+
+	public Node<E> getFirst() {
+		return first;
+	}
+
+}
+
+class Node<E> {
+	public Node(E e) {
+		// TODO Auto-generated constructor stub
+		data = e;
+	}
+
+	// The data stored in a mode of the list
+	private E data;
+///each node needsto point to the next node, we use this variable to do it
+	protected Node next;
 }
--- src/datastructure/list/ListTest.java
+++ src/datastructure/list/ListTest.java
@@ -1,5 +1,21 @@
 package datastructure.list;
 
 public class ListTest {
-
+	public static void main(String[] args) throws Exception {
+		Object obj1 = new Object();
+		Object obj2 = new Object();
+		System.out.println(obj1);
+		System.out.println(obj2);
+		List<Integer> list = new List<Integer>();
+		System.out.println( "The fist node of teh list is:"+ list.getFirst());
+		System.out.println("The last node of teh list is:" + list.getLast());
+		
+		list.add(new Node(10));
+		list.add(new Node(20));
+		list.add(new Node(40));
+		list.add(new Node(50));
+		list.add(new Node(30));
+	
+	
+	}
 }
